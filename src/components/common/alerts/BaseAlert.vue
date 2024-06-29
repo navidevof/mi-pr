956070d3a5ceeefb0b8fd8fa5081ed2139baaa2b
Ivan <ivan@elsociodigital.com>
2024-06-29 14:24:40 -0500

    Feature: add minimize alert

--- src/components/common/alerts/BaseAlert.vue
+++ src/components/common/alerts/BaseAlert.vue
@@ -1,5 +1,5 @@
 <template>
-  <aside class="fixed left-0 top-0 z-50 flex h-screen w-full items-center justify-center bg-black bg-opacity-60 transition">
+  <aside class="fixed left-0 top-0 z-50 flex h-screen w-full items-center justify-center bg-black bg-opacity-60 transition duration-300">
     <div class="flex w-full max-w-80 flex-col rounded-lg bg-custom-black-3 p-6 text-white">
       <slot></slot>
     </div>
